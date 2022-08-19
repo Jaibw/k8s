2fdd85266144d58c0d90cf08b5357f5d1a2051b7
Jaibw <jai.unix@gmail.com>
2022-08-19 11:40:58 +0530

    Create minikube-ubuntu20.04-podman.sh

---
+++ minikube-ubuntu20.04-podman.sh
@@ -0,0 +1,23 @@
+## Install Podman 
+sudo apt-get update 
+source /etc/os-release
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install podman
+podman --version
+
+## install minikube 
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
+minikube version
+
+## start minikube localcal cluster 
+minikube start --driver=podman 
+minikube kubectl -- get pods -A
+
+## install kubectl 
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl get pods -A
+kubectl get nodes
