003361f7176df5060e56c58c757b972f2aed34eb
Jaibw <jai.unix@gmail.com>
2022-12-06 22:21:13 +0530

    Update k8s-worker-ubuntu-20.04-docker.sh

--- k8s-worker-ubuntu-20.04-docker.sh
+++ k8s-worker-ubuntu-20.04-docker.sh
@@ -1,61 +1,42 @@
-# Install kubelet, kubeadm and kubectl
-sudo apt update
-sudo apt -y install curl apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt update
-sudo apt -y install vim git curl wget kubelet=1.23.7-00 kubeadm=1.23.7-00 kubectl=1.23.7-00
+sudo hostnamectl set-hostname k8s-node
+sudo apt update 
+sudo apt -y upgrade 
+sudo apt-get install ca-certificates curl  gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y 
+
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
-kubectl version --client && kubeadm version
+kubeadm version
+kubectl version --client 
 
-# Turn off swap 
-sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
-# Enable kernel modules
 sudo modprobe overlay
 sudo modprobe br_netfilter
-
-# Add some settings to sysctl
 sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 net.ipv4.ip_forward = 1
 EOF
 
-# Reload sysctl
 sudo sysctl --system
+sudo rm /etc/containerd/config.toml
+sudo systemctl restart containerd
 
-# install docker 
-# Add repo and Install packages
-sudo apt update
-sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install -y containerd.io docker-ce docker-ce-cli
+## sudo kubeadm join
 
-# Create required directories
-sudo mkdir -p /etc/systemd/system/docker.service.d
 
-# Create daemon json config file
-sudo tee /etc/docker/daemon.json <<EOF
-{
-  "exec-opts": ["native.cgroupdriver=systemd"],
-  "log-driver": "json-file",
-  "log-opts": {
-    "max-size": "100m"
-  },
-  "storage-driver": "overlay2"
-}
-EOF
 
-# Start and enable Services
-sudo systemctl daemon-reload 
-sudo systemctl restart docker
-sudo systemctl enable docker
 
-lsmod | grep br_netfilter
-sudo systemctl enable kubelet
 
-# Join the worker node 
